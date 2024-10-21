c876e2191ec5f5b79530e960785f8a8ef6839f24
FranFigueroa <70865088+FranFigueroa@users.noreply.github.com>
2024-10-20 21:23:33 -0300

    feat: Divide text into chunks

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 import streamlit as st
 import pdfplumber
+from langchain.text_splitter import CharacterTextSplitter
 
 # App title
 st.title("PDF BOT")
 
+
+# Text Splitter function
+def split_text(text, chunk_size = 1000, chunk_overlap = 200):
+    splitter = CharacterTextSplitter(
+        separator = "", # Divide for words
+        chunk_size = chunk_size, # Chunk size
+        chunk_overlap = chunk_overlap, # Superposition between chunks
+        length_function = len
+            )
+    chunks = splitter.split_text(text)
+    return chunks
+
 # Upload PDF Files
 uploaded_files = st.file_uploader("Upload your PDFs", type = "pdf", accept_multiple_files = True)
 
+
 if uploaded_files:
     for uploaded_file in uploaded_files:
-        st.write(f"Procesando:{uploaded_file.name} ")
-        with pdflumber.open(uploaded_file) as pdf:
+        st.write(f"Processing: {uploaded_file.name}")
+        with pdfplumber.open(uploaded_file) as pdf:
             text = ""
             for page in pdf.pages:
                 text += page.extract_text()
-                st.write(f"Content extract of PDF {uploaded_fie.naame}: ")
-                st.write(text)
+            
+            st.write(f"Conteth of PDF {uploaded_file.name}:")
+
+            # Dividir el texto en fragmentos
+            chunks = split_text(text)
+            st.write(f" The text has been divideded into {len(chunks)} chunks.")
+            
+            # Mostrar algunos fragmentos
+            for i, chunk in enumerate(chunks[:5]):  # Show some initial chunks
+                st.write(f"Chunks {i+1}:")
+                st.write(chunk)
